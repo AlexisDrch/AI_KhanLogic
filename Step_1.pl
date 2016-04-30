fff1e2d628215d97653383c5c7020f5c81679075
Alexis DUROCHER <Alexis DUROCHER>
2016-04-30 18:27:22 +0200

    up UI

--- Step_1.pl
+++ Step_1.pl
@@ -14,22 +14,38 @@ initLi(C, L, [X|Q]) :- X is (((2*C)+L) mod 3) +1,
 initBoard(L) :- init(6,6,L).
 
 /*Afficher le tableau*/
- affiche(_,0,Q,_) :- !.
- affiche(_,7,Q,_) :- !.
- affiche(C, L, [X|Q], 1) :- afficheLi(C, L, X, 1),
+
+ affiche(_,0,1,_) :- !.
+ affiche(C, L,1,[X|Q]) :- 
+					afficheLi(C, L,1,X),
 					nl,
 					Temp is L-1,
-					affiche(C, Temp, Q, 1).
+					affiche(C,Temp,1,Q).
 					
-afficheLi(0, _, _, _) :- !.
-afficheLi(C, L, [X|Q], 1) :- 
+afficheLi(0, _, 1, _) :- !.
+afficheLi(C, L,1,[X|Q]) :- 
 					write(X),
 					write(` `),
 					Temp is C-1,
-					afficheLi(Temp, L, Q).
+					afficheLi(Temp, L, 1, Q).
+/*affichage 3*/
+ affiche(_,0,3,_) :- !.
+ affiche(C, L,3,[X|Q]) :- 
+					Temp is L-1,
+					affiche(C,Temp,3,Q),
+					afficheLi(C, L,3,X),
+					nl.
+					
+afficheLi(0, _, 3, _) :- !.
+afficheLi(C, L, 3,[X|Q]) :- 
+					Temp is C-1,
+					afficheLi(Temp, L, 3, Q),
+					write(X),
+					write(` `).
+
 					
 					
-/* affichage 2 */
+/* affichage 2 
  affiche(C, L, [X|Q], 2) :- afficheLi(C, L, X, 2),
 					nl,
 					Temp is L-1,
@@ -40,15 +56,29 @@ afficheLi(C, L, [X|Q], 2) :-
 					afficheLi(Temp, L, Q, 2),
 					write(X),
 					write(` `).
+					
 
-/*Prédicat Afficha UI*/
-afficheBoard(L) :- affiche(6,6,L),
-					write('Entrez position')/*, read(X), write(X)*/.
-				
+ Prédicat Afficha UI*/
+
+afficheBoard(L) :- 	initBoard(L),
+					affiche(6,6,1,L),
+					nl,
+					write('Choisir une position : 1,2,3,4'),
+					nl,
+					write('---4---'),
+					nl,
+					write('2-----3'),
+					nl,
+					write('---1---'),
+					nl,
+					read(P), 
+					affiche(6,6,P,L),
+					!.
 					
 					
 					
-/* 
+					
+/*	
 
  3
 2 4
